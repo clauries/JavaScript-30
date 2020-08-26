fcc378b392106ec63621f7b39acb63aa5cc0a097
clauries <55369403+clauries@users.noreply.github.com>
2020-08-25 19:00:53 -0700

    Added basic content to web page

---
+++ 02-Clock/assets/app.js
@@ -0,0 +1,31 @@
+const hands = document.querySelector('.hand');
+const secondHand = document.querySelector('.second-hand');
+const minuteHand = document.querySelector('.min-hand');
+const hourHand = document.querySelector('.hour-hand');
+
+function setDate() {
+    const now = new Date();
+
+    const seconds = now.getSeconds();
+    const secondsDegrees = ((seconds / 60) * 360) + 90;
+    secondHand.style.transform = `rotate(${secondsDegrees}deg)`;
+    
+    const minutes = now.getMinutes();
+    const minutesDegrees = ((minutes / 60) * 360) + ((seconds/60)*6) + 90;
+    minuteHand.style.transform = `rotate(${minutesDegrees}deg)`;
+
+    const hours = now.getHours();
+    const hoursDegrees = ((hours / 12) * 360) + ((minutes/60)*30) + 90;
+    hourHand.style.transform = `rotate(${hoursDegrees}deg)`;
+
+    if (seconds === 59) {
+        hands.style.transition = `none`;
+        console.log("hands", seconds);
+    }
+
+    if (seconds === 1) {
+        hands.style.transition = `all 0.05s`;
+    }
+}
+
+setInterval(setDate, 1000);
